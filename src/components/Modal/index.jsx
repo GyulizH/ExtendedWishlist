c5f4d4b3765c68c09ad724d250123e9aefd36f9f
hsahin <hilmitolgasahin@pvh.com>
2020-08-10 22:12:59 +0200

    fix: checkboxes

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,138 +1,145 @@
-import React from 'react';
-import "./Modal.scss"
-import {TOGGLE_MODAL} from "../../store/Modal/action";
+import React from 'react'
+import './Modal.scss'
+import { TOGGLE_MODAL } from '../../store/Modal/action'
 import { connect } from 'react-redux'
-import Button, {BTN_WITH_PLUS_ICON,BTN_WITH_CROSS_ICON} from "../Button";
-import {addNewCombination,toggleCombinationCheckbox} from "../../store/Modal/combinationAction";
-import Editable from "../Editable/Editable";
+import Button, { BTN_WITH_PLUS_ICON, BTN_WITH_CROSS_ICON } from '../Button'
+import {
+  addNewCombination,
+  toggleCombinationCheckbox,
+} from '../../store/Modal/combinationAction'
+import Editable from '../Editable/Editable'
 
-
-class Modal extends React.Component{
-    constructor(props) {
-        super(props);
-        this.closeModal = this.closeModal.bind(this)
-        this.state = {
-            isAddNewCombination : false,
-            combinationTitle:''
-        }
+class Modal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.closeModal = this.closeModal.bind(this)
+    this.state = {
+      isAddNewCombination: false,
+      combinationTitle: '',
     }
-//do I need this?
-    componentDidUpdate(prevProps) {
-        if (prevProps.combinationList !== this.props.combinationList) {
-        }
+  }
+  //do I need this?
+  componentDidUpdate(prevProps) {
+    if (prevProps.combinationList !== this.props.combinationList) {
     }
+  }
 
-//I might need to order combinations by id meaning date
-    sendNewCombinationForm= (e) => {
-        let newCombination = {
-            id:Date.now(),
-            name:this.state.combinationTitle,
-            product: this.props.selectedProduct,
-            checked:false
-        }
-        this.props.addNewCombination(newCombination)
-        this.setState({isAddNewCombination: false})
-        e.preventDefault()
+  //I might need to order combinations by id meaning date
+  sendNewCombinationForm = (e) => {
+    let newCombination = {
+      id: Date.now(),
+      name: this.state.combinationTitle,
+      product: this.props.selectedProduct,
+      checked: false,
     }
+    this.props.addNewCombination(newCombination)
+    this.setState({ isAddNewCombination: false })
+    e.preventDefault()
+  }
 
-    closeModal(){
-        this.props.closeModal()
-    }
+  closeModal() {
+    this.props.closeModal()
+  }
 
-    openAddNewCombinationBox = () => {
-        this.setState({isAddNewCombination: true})
-    }
-    addEditableCombinationField = () => {
-        return(
-                <Editable
-                    text={this.state.combinationTitle}
-                    type="text"
-                    isNew={true}
-                >
-                    <form onSubmit={this.sendNewCombinationForm}>
-                        <input
-                            type="text"
-                            onChange={e => {this.setState({combinationTitle : e.target.value})}}
-                            className="Modal-Editable-Input"
-                        />
-                    </form>
-                </Editable>
-        )
-    }
+  openAddNewCombinationBox = () => {
+    this.setState({ isAddNewCombination: true })
+  }
+  addEditableCombinationField = () => {
+    return (
+      <Editable text={this.state.combinationTitle} type="text" isNew={true}>
+        <form onSubmit={this.sendNewCombinationForm}>
+          <input
+            type="text"
+            onChange={(e) => {
+              this.setState({ combinationTitle: e.target.value })
+            }}
+            className="Modal-Editable-Input"
+          />
+        </form>
+      </Editable>
+    )
+  }
 
-    toggleCheckBox=(e) => {
-         e.preventDefault()
-        if(e.target.type === 'checkbox'){
-            let  fromStrToNumber = parseInt(e.target.value,10)
-            this.props.toggleCombinationCheckbox(fromStrToNumber)
-        }
-}
+  toggleCheckBox = (e) => {
+    let fromStrToNumber = parseInt(e.target.id, 10)
+    this.props.toggleCombinationCheckbox(fromStrToNumber)
+  }
 
-    //add go to combination button to each combination
-    //redux and onsubmit onchange
-    //onclickling outside the input field return to the old name
-    render() {
-        return(
+  //add go to combination button to each combination
+  //redux and onsubmit onchange
+  //onclickling outside the input field return to the old name
+  render() {
+    return (
+      <div>
+        <div onClick={this.closeModal} className="mask"></div>
+        <div className="WishListModal">
+          <div className="WishListModal--Content">
             <div>
-            <div onClick={this.closeModal} className="mask"></div>
-            <div className="WishListModal">
-                  <div className="WishListModal--Content">
-                      <div>
-                      {this.props.combinationList.length> 0 &&
-                      <div className="WishListModal-Header">
-                          <label className="WishListModal--Title">ADD TO... </label>
-                          <Button  className="WishList-Close-Button" onClick={this.closeModal} variant={BTN_WITH_CROSS_ICON}></Button>
-                      </div>}
-                      <ul className="WishListModal-List">
-                      {this.props.combinationList.map(combination => {
-                          return (
-                              <li className="WishList-Combination-List-Item" key={combination.id}>
-                                  <label className="checkboxContainer">
-                                      <input
-                                          type="checkbox"
-                                          id={combination.id}
-                                          checked={combination.checked}
-                                          onChange={this.toggleCheckBox}
-                                      />
-                                      <span className="checkmark"></span>
-                                      {combination.name}
-                                  </label>
-                              </li>
-                          )
-                      })}
-                      </ul>
-                      <div>
-                          {this.state.isAddNewCombination? this.addEditableCombinationField():
-                              <Button
-                                  variant={BTN_WITH_PLUS_ICON}
-                                  onClick={this.openAddNewCombinationBox}
-                                  className="WishList-Plus-Button"
-                              >
-                                  Add New Combination
-                              </Button>}
-                      </div>
-                      </div>
-                  </div>
+              {this.props.combinationList.length > 0 && (
+                <div className="WishListModal-Header">
+                  <label className="WishListModal--Title">ADD TO... </label>
+                  <Button
+                    className="WishList-Close-Button"
+                    onClick={this.closeModal}
+                    variant={BTN_WITH_CROSS_ICON}
+                  ></Button>
+                </div>
+              )}
+              <ul className="WishListModal-List">
+                {this.props.combinationList.map((combination) => {
+                  return (
+                    <li
+                      className="WishList-Combination-List-Item"
+                      key={combination.id}
+                    >
+                      <label className="checkboxContainer">
+                        <input
+                          type="checkbox"
+                          id={combination.id}
+                          checked={combination.checked}
+                          onChange={this.toggleCheckBox}
+                        />
+                        <span className="checkmark"></span>
+                        {combination.name}
+                      </label>
+                    </li>
+                  )
+                })}
+              </ul>
+              <div>
+                {this.state.isAddNewCombination ? (
+                  this.addEditableCombinationField()
+                ) : (
+                  <Button
+                    variant={BTN_WITH_PLUS_ICON}
+                    onClick={this.openAddNewCombinationBox}
+                    className="WishList-Plus-Button"
+                  >
+                    Add New Combination
+                  </Button>
+                )}
+              </div>
             </div>
-            </div>
-            )
-    }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-const mapDispatchToProps= dispatch => {
-    return {
-        closeModal : () => dispatch({type: TOGGLE_MODAL}),
-        addNewCombination: (combination) => dispatch(addNewCombination(combination)),
-        toggleCombinationCheckbox:(id) => dispatch(toggleCombinationCheckbox(id))
-
-    }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    closeModal: () => dispatch({ type: TOGGLE_MODAL }),
+    addNewCombination: (combination) =>
+      dispatch(addNewCombination(combination)),
+    toggleCombinationCheckbox: (id) => dispatch(toggleCombinationCheckbox(id)),
+  }
 }
 
-const mapStateToProps = state => ({
-    isModalOpen: state.modal.isModalOpen,
-    selectedProduct:state.modal.selectedProduct,
-    combinationList: state.combinations
+const mapStateToProps = (state) => ({
+  isModalOpen: state.modal.isModalOpen,
+  selectedProduct: state.modal.selectedProduct,
+  combinationList: state.combinations,
 })
 
-export default connect(mapStateToProps,mapDispatchToProps)(Modal)
-
+export default connect(mapStateToProps, mapDispatchToProps)(Modal)
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx']
     },
-    devtool: "source-map",
+    devtool: "eval-source-map",
     module: {
         rules: [
             {
