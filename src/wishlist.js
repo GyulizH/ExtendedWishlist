dd2c4920098287ed371dfd7a3adcc9c0e0f1b0d3
Gyuliz Hadzhieva <fg.hadzhieva@gmail.com>
2020-08-10 21:16:43 +0200

    virgul

--- src/wishlist.js
+++ src/wishlist.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const combinationList = document.createElement('div')
-
 window.onload = function () {
    console.log("wishlist loaded")
    combinationList.className = "wishlist--combinationlist-wrapper"
