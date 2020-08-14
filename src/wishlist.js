c36d6090467a0744954f94faa3e5b121138c99d8
Gyuliz Hadzhieva <fg.hadzhieva@gmail.com>
2020-08-14 16:59:43 +0200

    combination list on wishlist

--- src/wishlist.js
+++ src/wishlist.js
@@ -7,18 +7,25 @@ const combinationList = document.createElement('div')
 window.onload = function () {
   combinationList.className = 'wishlist--combinationlist-wrapper'
   combinationList.innerHTML = 'hey there how are you'
-  // let nodes = document.querySelectorAll(".wishlist__listing > *")
-  // let arr = Array.from(nodes)
-  // // document.body.getElementsByClassName("wishlist__listing")[0].innerHTML = " "
+  let nodes = document.querySelectorAll('.wishlist__listing > *')
+  let arr = Array.from(nodes)
+  // document.body.getElementsByClassName("wishlist__listing")[0].innerHTML = " "
   // Array.prototype.insert = function ( index, item ) {
-  //   this.splice( index, 0, item );
+  // this.splice( index, 0, item );
   // }
   // arr.insert(2,combinationList)
   // arr.forEach(function(item){
-  //   document.getElementsByClassName("wishlist__listing")[0].appendChild(item.cloneNode(true));
+  // document.getElementsByClassName("wishlist__listing")[0].appendChild(item.cloneNode(true));
   // });
-  document
-    .getElementsByClassName('wishlist__listing')[0]
-    .appendChild(combinationList)
+  // document
+  //   .getElementsByClassName('wishlist__listing')[0]
+  //   .appendChild(combinationList)
+  let toBeInsertedBefore = document.getElementsByClassName(
+    'wishlist__listing'
+  )[0]
+  toBeInsertedBefore.insertBefore(
+    combinationList,
+    toBeInsertedBefore.childNodes[3]
+  )
   ReactDOM.render(<CombinationList />, combinationList)
 }
